43a33f024d68c09e6705b17041be9e1af25f76e3
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2024-11-07 11:44:36 -0300

    feat: ensure transaction can be updated

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -7,23 +7,23 @@ interface TransactionTypeBadgeProps {
 const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   if (transaction.type === TransactionType.DEPOSIT) {
     return (
-      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
-        <CircleIcon className="mr-2 fill-primary" size={10} />
+      <Badge className="bg-green-950/30 font-bold text-green-500 hover:bg-green-950/30">
+        <CircleIcon className="mr-2 fill-green-500" size={10} />
         Depósito
       </Badge>
     );
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="font bold bg-danger text-danger bg-opacity-10">
-        <CircleIcon className="fill-danger mr-2" size={10} />
+      <Badge className="bg-red-950/30 font-bold text-red-500 hover:bg-red-950/30">
+        <CircleIcon className="mr-2 fill-red-500" size={10} />
         Despesa
       </Badge>
     );
   }
   return (
-    <Badge className="font bold bg-white bg-opacity-10 text-white">
-      <CircleIcon className="mr-2 fill-white" size={10} />
+    <Badge className="bg-gray-800/50 font-bold text-gray-300 hover:bg-gray-800/50">
+      <CircleIcon className="mr-2 fill-gray-300" size={10} />
       Investimento
     </Badge>
   );
